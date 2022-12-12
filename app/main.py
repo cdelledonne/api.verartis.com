afc233399a9ec9ff5d9699223f802ab547e6c509
Carlo Delle Donne <carlo.delle.donne@gmail.com>
2022-12-12 12:41:51 +0100

    Add (boilerplate) API endpoint for contact form

--- app/main.py
+++ app/main.py
@@ -1,15 +1,36 @@
-from typing import Union
+import os
 
 from fastapi import FastAPI
+from fastapi_versioning import VersionedFastAPI, version
+from pydantic import BaseModel, Field
+
+
+class ContactForm(BaseModel):
+    first_name: str
+    last_name: str
+    email_address: str
+    message: str
+    hcaptcha_response: str = Field(alias="h-captcha-response")
+    template: str | None = Field(default=None, alias="_template")
+    next: str | None = Field(default=None, alias="_next")
+    captcha: str | None = Field(default=None, alias="_captcha")
+    honey: str | None = Field(default=None, alias="_honey")
+
 
 app = FastAPI()
 
 
 @app.get("/")
+@version(1)
 def read_root():
-    return {"Hello": "World"}
+    return {"application": "Ver Artis API"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.post("/contactform")
+@version(1)
+def send_contact_form(form: ContactForm):
+    secret_key = os.getenv("HCAPTCHA_SECRET_KEY")
+    return form
+
+
+app = VersionedFastAPI(app, version_format="{major}", prefix_format="/v{major}")
