d088b731b66fe43d5eaa244ffecb8c49811431e4
Carlo Delle Donne <carlo.delle.donne@gmail.com>
2022-12-13 13:52:20 +0100

    Add actual API endpoint for contact form

--- app/main.py
+++ app/main.py
@@ -1,23 +1,19 @@
+import json
+import logging
 import os
 
-from fastapi import FastAPI
+import requests
+from fastapi import FastAPI, Form, HTTPException
 from fastapi_versioning import VersionedFastAPI, version
-from pydantic import BaseModel, Field
 
+logger = logging.getLogger("uvicorn")
 
-class ContactForm(BaseModel):
-    first_name: str
-    last_name: str
-    email_address: str
-    message: str
-    hcaptcha_response: str = Field(alias="h-captcha-response")
-    template: str | None = Field(default=None, alias="_template")
-    next: str | None = Field(default=None, alias="_next")
-    captcha: str | None = Field(default=None, alias="_captcha")
-    honey: str | None = Field(default=None, alias="_honey")
-
-
-app = FastAPI()
+app = FastAPI(
+    title="Ver Artis API",
+    openapi_url=None,
+    docs_url=None,
+    redoc_url=None,
+)
 
 
 @app.get("/")
@@ -28,9 +24,66 @@ def read_root():
 
 @app.post("/contactform")
 @version(1)
-def send_contact_form(form: ContactForm):
-    secret_key = os.getenv("HCAPTCHA_SECRET_KEY")
-    return form
+def send_contact_form(
+    first_name: str = Form(),
+    last_name: str = Form(),
+    email_address: str = Form(),
+    message: str = Form(),
+    hcaptcha_response: str = Form(alias="h-captcha-response"),
+    origin: str = Form(alias="_origin"),
+    next_on_success: str = Form(alias="_next_on_success"),
+    next_on_failure: str = Form(alias="_next_on_failure"),
+):
+    # Prepare POST request to verify hCaptcha response with the API endpoint
+    hcaptcha_url = "https://hcaptcha.com/siteverify"
+    hcaptcha_data = {
+        "response": hcaptcha_response,
+        "secret": os.getenv("HCAPTCHA_SECRET_KEY"),
+        "sitekey": os.getenv("HCAPTCHA_SITEKEY"),
+    }
+    hcaptcha_headers = {
+        "Content-Type": "application/x-www-form-urlencoded",
+    }
+
+    # Prepare POST request to submit form
+    formsubmit_url = "https://formsubmit.co/ajax/{}".format(
+        os.getenv("FORMSUBMIT_STRING")
+    )
+    formsubmit_data = {
+        "first_name": first_name,
+        "last_name": last_name,
+        "email_address": email_address,
+        "message": message,
+        "_template": "box",
+        "_captcha": "false",
+    }
+    formsubmit_headers = {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+        "Origin": origin,
+        "Referer": origin,
+    }
+
+    # Submit POST request to hCaptcha API endpoint
+    response = requests.post(hcaptcha_url, data=hcaptcha_data, headers=hcaptcha_headers)
+    hcaptcha_verification_response_json = response.json()
+
+    # If hCaptcha is valid, actually submit form
+    if hcaptcha_verification_response_json["success"]:
+        response = requests.post(
+            formsubmit_url, data=json.dumps(formsubmit_data), headers=formsubmit_headers
+        )
+        form_submission_response_json = response.json()
+        logger.info("Form submission posted")
+        logger.info(form_submission_response_json)
+        location = next_on_success
+    else:
+        logger.error("hCaptcha verification failure")
+        logger.error(hcaptcha_verification_response_json)
+        location = next_on_failure
+
+    # Return a 302 error to redirect client to specified location
+    raise HTTPException(status_code=302, detail="Found", headers={"Location": location})
 
 
 app = VersionedFastAPI(app, version_format="{major}", prefix_format="/v{major}")
